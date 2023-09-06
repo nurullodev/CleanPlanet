89e776221c0c2edf8740d9992dd88d39a4f4d0e0
Nurullo <ghostone234qwer.gmail.com>
2023-09-07 02:26:35 +0500

    Clean Planet project

---
+++ src/CleanPlanet.Wep/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanPlanet.Wep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
