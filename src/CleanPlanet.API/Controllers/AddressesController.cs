ca9d67712533f9c32138cdfcd1ba2ae25eb89c67
Nurullo <ghostone234qwer@gmail.com>
2023-09-16 01:50:14 +0500

    update database

--- src/CleanPlanet.API/Controllers/AddressesController.cs
+++ src/CleanPlanet.API/Controllers/AddressesController.cs
@@ -1,5 +1,12 @@
-﻿namespace CleanPlanet.API.Controllers;
+﻿using CleanPlanet.Service.Interfaces;
+
+namespace CleanPlanet.API.Controllers;
 
 public class AddressesController : BaseController
 {
+    private readonly IAddressService addressService;
+    public AddressesController(IAddressService addressService)
+    {
+        this.addressService = addressService;
+    }
 }
